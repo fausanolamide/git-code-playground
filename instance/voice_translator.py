f3d8307b71e3baf68643b29614fbd537556ca7b8
fausan olamide <fausanolamide05@gmail.com>
2023-12-11 10:00:37 +0100

    upto date

--- instance/voice_translator.py
+++ instance/voice_translator.py
@@ -1,17 +1,23 @@
 import speech_recognition as sr
-from google_trans_new import google_translator
+
 r = sr.Recognizer()
 
 with sr.Microphone() as source:
-    print('speak now')
-    audio_text = r.listen(source)
+    print('Speak now...')
     try:
+        # Adjust the timeout value as needed
+        audio_text = r.listen(source, timeout=5)
         speech_text = r.recognize_google(audio_text)
-        print(speech_text)
+        print('Speech recognized:', speech_text)
+    except sr.WaitTimeoutError:
+        print('Speech recognition timed out. No speech detected.')
     except sr.UnknownValueError:
-        print("Could not understand")
-    except sr.RequestError:
-        print("Unable to get result from google")
+        print('Could not understand audio')
+    except sr.RequestError as e:
+        print(
+            f'Unable to get result from Google Speech Recognition service; {e}')
+
+# Rest of your code...
 
     # tranlated_text = google_translator.translate(speech_text, lang_tgt="fr")
     # print(tranlated_text)
